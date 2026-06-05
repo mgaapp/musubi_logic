16e08980bea73a8d79047e015cc0f781b1945799
Ubuntu <ubuntu@ip-172-31-44-127.ap-northeast-1.compute.internal>
2026-06-05 13:26:41 +0000

    勘定科目にURL追加から

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,18 @@
+class Admin::CategoriesController < ApplicationController
+    def new
+      @category=Category.new
+end
+
+    def create
+      @category=Category.new(category_params)
+      if @category.save
+        redirect_to root_path, notice"勘定科目を追加しました"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :account_item)
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   namespace :admin do
    resources :users
    resources :requests, only: [:index, :show, :update]
+   resources :categories, only: [:new, :create]
   end
   scope module: :public do
     resources :requests, only: [:index, :show, :new, :create]
