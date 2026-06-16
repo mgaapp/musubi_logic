6bb985e62018adba0d52bab51695553f00edf01e
Ubuntu <ubuntu@ip-172-31-44-127.ap-northeast-1.compute.internal>
2026-06-16 13:50:43 +0000

    キーワード検索機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
+ before_action :admin_user, only: [:update]
+  
   def index
+    if params[:filter] == "unanswered"
+      @posts = Post.where(answer: [nil, ""]).or(Post.where(is_public: false))
+    elsif params[:filter] == "answered"
+      @posts = Post.where.not(answer: [nil, ""]).where(is_public: true)
+    else
     @posts = Post.all 
   end
+end
    def new
     @post = Post.new
    end
@@ -26,7 +34,13 @@ class PostsController < ApplicationController
   end
  private
 
+ def admin_user
+  if Current.user.nil? || !Current.user.admin?
+    redirect_to root_path, alert: "管理者専用の機能です。アクセス権限がありません。"
+  end
+end
+
  def post_params
-   params.require(:post).permit(:title, :content, :answer)
+   params.require(:post).permit(:title, :content, :answer, :is_public)
  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_15_122944) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_16_130410) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -90,6 +90,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_15_122944) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "answer"
+    t.boolean "is_public"
   end
 
   create_table "requests", force: :cascade do |t|
