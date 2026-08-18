87962ef3ae52edd3697a9586373257cb448f56da
Ubuntu <ubuntu@ip-172-31-44-127.ap-northeast-1.compute.internal>
2026-08-18 12:12:20 +0000

    インデント調整

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -1,36 +1,36 @@
 class Admin::AssetsController < ApplicationController
-  def index
+  def index 
     @assets = Asset.includes(request: :category).all
 
     respond_to do |format|
-      format.html
+      format.html 
       format.csv do
-        send_data generate_csv(@assets), filename: "assets-#{Date.today}.csv", type: "text/csv; charset=utf-8"
+        send_data generate_csv(@assets), filename: "assets-#{Date.today}.csv", type: "text/csv; charset=utf-8" 
       end
     end
   end
 
-  def show
-    @asset = Asset.find(params[:id])
+  def show 
+    @asset = Asset.find(params[:id]) 
   end
 
-  def destroy
-    @asset = Asset.find(params[:id])
+  def destroy 
+    @asset = Asset.find(params[:id]) 
     @asset.destroy
-    redirect_to admin_assets_path, notice: "固定資産を削除しました。"
+    redirect_to admin_assets_path, notice: "固定資産を削除しました。" 
   end
 
   private
 
   def generate_csv(assets)
-    require 'csv'
+    require 'csv' 
 
-    bom = "\uFEFF"
-    CSV.generate(bom) do |csv|
-      csv << ["資産ID", "品名", "拠点名", "取得金額（税抜）", "耐用年数", "償却方法", "供用開始日", "償却終了予定日", "購入先", "購入日", "状態"]
+    bom = "\uFEFF" 
+    CSV.generate(bom) do |csv| 
+      csv << ["資産ID", "品名", "拠点名", "取得金額（税抜）", "耐用年数", "償却方法", "供用開始日", "償却終了予定日", "購入先", "購入日", "状態"] 
 
-      assets.each do |asset|
-        csv << [
+      assets.each do |asset| 
+        csv << [ 
           asset.id,
           asset.request&.category&.name,
           asset.location,
