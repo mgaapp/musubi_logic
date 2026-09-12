b76f59f860c8cc50b8febf1495cad06a53d75a37
Ubuntu <ubuntu@ip-172-31-44-127.ap-northeast-1.compute.internal>
2026-09-12 11:39:57 +0000

    モデルファイルにコメントを追加

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,9 +1,11 @@
 class Request < ApplicationRecord
+  # ユーザーやカテゴリ、他機能とのデータ連携
   belongs_to :user
   belongs_to :category
   has_one :inventory, dependent: :destroy
   has_one_attached :image
 
+  # 申請内容の入力チェック
   validates :user_id, presence: true
   validates :category_id, presence: true
   validates :total_amount_incl_tax, presence: true
